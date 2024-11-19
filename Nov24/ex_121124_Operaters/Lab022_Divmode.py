6c2eebf97161b08cc6520db925de718c855ebf5e
Mopuri Babavali <gprecbabavali981@gmail.com>
2024-11-19 23:26:59 +0530

    hhh

---
+++ Nov24/ex_121124_Operaters/Lab022_Divmode.py
@@ -0,0 +1,3 @@
+q, r = divmod(5, 2)
+print(q)  # quoitiant
+print(r)  # reminder
---
+++ Nov24/ex_121124_Operaters/Lab023_Increment_decrement_operator.py
@@ -0,0 +1,15 @@
+x = 5
+x += 1  # x=x+1
+print(x)
+
+x = 5
+x -= 1  # x=x-1
+print(x)
+
+x = 5
+x *= 1  # x=x*1
+print(x)
+
+x = 5
+x %= 1  # x=x%1
+print(x)
