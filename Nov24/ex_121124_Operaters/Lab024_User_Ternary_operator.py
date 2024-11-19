f9412ad3efb1c8c488fbffe9ae77b3632148024a
Mopuri Babavali <gprecbabavali981@gmail.com>
2024-11-19 23:43:23 +0530

    hhh

---
+++ Nov24/ex_121124_Operaters/Lab024_User_Ternary_operator.py
@@ -0,0 +1,9 @@
+# program if age > 18 then allow to vote
+# otherwise = no
+
+user_age = int(input("Enter your age \n"))
+
+if user_age > 18:
+    print("Yes you can GO=> GOA")
+else:
+    print("Not allow")
