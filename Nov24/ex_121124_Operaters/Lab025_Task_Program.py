02eceb8c7c973980bd1aaf205adff3ef798e026f
Mopuri Babavali <gprecbabavali981@gmail.com>
2024-11-20 23:03:13 +0530

    hhh

---
+++ Nov24/ex_121124_Operaters/Lab025_Task_Program.py
@@ -0,0 +1,9 @@
+# write a program to take a user age and let him know if he can go club 21
+
+# other wise no
+
+age = int(input("Enter your age :\n "))
+if age >= 21:
+    print("your allow to enter the club")
+else:
+    print("Not allow age should be 21")
---
+++ Nov24/ex_121124_Operaters/Lab026.py
@@ -0,0 +1,5 @@
+user = int(input(" Enter the AGE :\n "))
+if user >=18:
+    print("YOU ARE ELIGIBLE FOR CLUB")
+else:
+    print("YOU NOT ARE ELIGIBLE FOR CLUB")
