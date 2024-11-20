6513721a737e6a5bf3208e8389c2c34781271573
Mopuri Babavali <gprecbabavali981@gmail.com>
2024-11-20 23:14:01 +0530

    hhh

---
+++ Nov24/ex_121124_Operaters/Lab027_Task3.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter the number num1 :  \n"))
+num2 = int(input("Enter the number num2 :  \n"))
+
+if num1 > num2:
+    print("Max num is: ", num1)
+else:
+    print("Max num is: ", num2)
