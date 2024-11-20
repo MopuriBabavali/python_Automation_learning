421556799405165bba96fe4a7195a9f8912e637d
Mopuri Babavali <gprecbabavali981@gmail.com>
2024-11-20 23:10:55 +0530

    hhh

--- Nov24/ex_121124_Operaters/Lab026.py
+++
@@ -1,5 +0,0 @@
-user = int(input(" Enter the AGE :\n "))
-if user >=18:
-    print("YOU ARE ELIGIBLE FOR CLUB")
-else:
-    print("YOU NOT ARE ELIGIBLE FOR CLUB")
---
+++ Nov24/ex_121124_Operaters/Lab026_Task2.py
@@ -0,0 +1,5 @@
+a_value = int(input("Enter your a value :\n "))
+if a_value == 10:
+    print("Hello world")
+else:
+    print(" not allow")
